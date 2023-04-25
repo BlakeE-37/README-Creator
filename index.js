c4f12c49442d8684a2d37507a56dcd25c397ef88
Blake H <blake.hollingsworth80@gmail.com>
2023-04-25 13:26:07 -0600

    added the first couple of questions for the inquirer prompt

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 const fs = require = ('fs')
 
 // TODO: Create an array of questions for user input
-const questions = ['What is the title of your project?'];
+// const licenses = Choices(["Choice A", "choice B"])
+const questions = ['What is the title of your project?', 'Write a short description of your project:', 'Chose a License'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -12,7 +13,33 @@ function writeToFile(fileName, data) {
     )
 }
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions[0],
+                name: 'title',
+            },
+            {
+                type: 'editor',
+                message: questions[1],
+                name: 'description',
+            },
+            {
+                type: 'list',
+                choices: ['MIT License', 'Other License'],
+                message: questions[2],
+                name: 'license',
+            },
+        ])
+        .then((response) => {
+            console.log(response.title);
+            console.log(response.description);
+            console.log(response.license);
+        })
+
+}
 
 // Function call to initialize app
 init();
