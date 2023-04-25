a4465bb5c3a3eba57dd206fa5e2ba69912f60d6e
Blake H <blake.hollingsworth80@gmail.com>
2023-04-25 13:33:38 -0600

    added table of contents to the readme

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Professional README Generator
 ## Description
+
 ## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
 ## Installation
 ## Usage
 ## License
 ## Contributing
 ## Tests
+## Questions
 ## Questions
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,15 +13,23 @@ function renderLicenseSection(license) { }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.title}
-  ## Description
-  ## Table of Contents
-  ## Installation
-  ## Usage
-  ## License
-  ## Contributing
-  ## Tests
-  ## Questions
+# ${data.title}
+## Description
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+## Usage
+## License
+## Contributing
+## Tests
+## Questions
 `;
 }
 
