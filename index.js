a21d6a2473b4e1565108a31888fe8f0f13e33b25
Blake H <blake.hollingsworth80@gmail.com>
2023-04-25 16:39:10 -0600

    small edits

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function init() {
             },
             {
                 type: 'list',
-                choices: ['MIT License', 'IBM', 'ISC', 'Eclipse', 'Creative Commons', 'Apache', 'Mozilla', 'Perl', new inquirer.Separator(), 'none'],
+                choices: ['MIT License', 'IBM', 'ISC', 'Eclipse', 'Creative Commons', 'Apache', 'Mozilla', 'Perl', new inquirer.Separator(), 'none', new inquirer.Separator()],
                 message: questions[4],
                 name: 'license',
             },
